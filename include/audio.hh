e864892b28af4ab597bbb6a8d4c34bfad04316bc
John Francis <john9francis@gmail.com>
2024-05-07 23:33:57 -0400

    Changed the .mp3 to a .wav and started audio.hh file

---
+++ include/audio.hh
@@ -0,0 +1,16 @@
+#ifndef AUDIO_PLAYER_HH
+#define AUDIO_PLAYER_HH
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_audio.h>
+
+class AudioPlayer{
+public:
+  AudioPlayer();
+  ~AudioPlayer();
+
+private:
+}
+
+
+#endif
